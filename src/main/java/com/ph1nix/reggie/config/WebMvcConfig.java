07c4b929ed0adc7f0f830a77e6614856dcf41df1
Huayu Zhang <zhanghuayu.dev@gmail.com>
2022-07-15 17:36:20 +0100

    Initial commit

---
+++ src/main/java/com/ph1nix/reggie/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.ph1nix.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * Set static resource mapping
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("Mapping Static resources...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
